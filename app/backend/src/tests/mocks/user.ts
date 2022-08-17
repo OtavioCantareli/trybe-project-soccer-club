af0e56960cba2fd5f25ab6683ac9a4189310bcb3
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-17 18:55:22 -0300

    User mock complete

---
+++ app/backend/src/tests/mocks/user.ts
@@ -0,0 +1,11 @@
+import InterfaceUser from "../../interfaces/InterfaceUser";
+
+const userMock: InterfaceUser = {
+  username: "xablau",
+  role: "admin",
+  id: 1,
+  email: "xablau@email.com",
+  password: "xablautop",
+};
+
+export default userMock;
