25c813c150b970e4dc5dc2a56d2be612d510905d
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-17 19:21:25 -0300

    User login tests

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -1,23 +1,55 @@
 import * as sinon from "sinon";
 import * as chai from "chai";
-import chaiHttp from "chai-http";
+// @ts-ignore
+import chaiHttp = require("chai-http");
+
 import { app } from "../app";
 import User from "../database/models/UserModel";
-import userLogin from "./mocks/userLogin";
-import * as jwt from "jsonwebtoken";
-import userMock from "./mocks/user";
+import IntUser from "../interfaces/User";
+
+import { Response } from "superagent";
+
+chai.use(chaiHttp);
+
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe("LOGIN", () => {
-  describe("Works", () => {
-    it("Returns status 200", async () => {
-      sinon.stub(User, "findOne").resolves(userMock as unknown as User);
-      const reponse = await chai.request(app).post("/login").send(userLogin);
-    });
-    it("Returns token", () => {
-      sinon.stub(jwt, "sign").returnsThis();
-    });
+  let chaiHttpResponse: Response;
+
+  const userMock: IntUser = {
+    id: 1,
+    email: "email@test.com",
+    password: "123456",
+    role: "user",
+    username: "username",
+  };
+
+  before(async () => {
+    sinon.stub(User, "findOne").resolves(userMock as User);
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it("Returns status 200", async () => {
+    chaiHttpResponse = await chai.request(app).post("/login");
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+
+  it("Returns token", async () => {
+    chaiHttpResponse = await chai.request(app).post("/login");
+
+    expect(chaiHttpResponse.body).to.be.an("object").with.key("token");
   });
 });
+function before(arg0: () => Promise<void>) {
+  throw new Error("Function not implemented.");
+}
+
+function after(arg0: () => void) {
+  throw new Error("Function not implemented.");
+}
