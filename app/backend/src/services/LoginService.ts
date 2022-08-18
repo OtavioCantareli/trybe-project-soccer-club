66d36f34c6a61bf8512eacc63ef2923c50859126
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-18 10:34:02 -0300

    Login service

---
+++ app/backend/src/services/LoginService.ts
@@ -0,0 +1,17 @@
+import * as bcrypt from 'bcryptjs';
+import Token from '../utils/Token';
+import User from '../database/models/UserModel';
+import IntUser from '../interfaces/User';
+
+class LoginService {
+  login = async ({ email, password }: IntUser) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return { code: 401, message: 'Incorrect email or password' };
+    const correctPass = bcrypt.compareSync(password, user.password);
+    if (!correctPass) { return { code: 401, message: 'Incorrect email or password' }; }
+    const token = Token(user);
+    return { code: 200, token };
+  };
+}
+
+export default new LoginService();
