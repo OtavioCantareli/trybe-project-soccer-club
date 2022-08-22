df03338e4790c18c25881e22ae9de52e58ca95de
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-22 13:21:28 -0300

    In progress filter

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -3,7 +3,16 @@ import MatchService from '../services/MatchService';
 
 class MatchController {
   matches = async (request: Request, response: Response) => {
+    const { inProgress } = request.query;
     const matches = await MatchService.matches();
+    if (inProgress !== undefined) {
+      if (inProgress) {
+        const inProgressMatches = matches.filter((match) => match.inProgress);
+        return response.status(200).json(inProgressMatches);
+      }
+      const finishedMatches = matches.filter((match) => !match.inProgress);
+      return response.status(200).json(finishedMatches);
+    }
     return response.status(200).json(matches);
   };
 }
