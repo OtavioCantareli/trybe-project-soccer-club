69fecf2eb5842f7acf23f0df3da8797b560038f5
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-24 15:49:33 -0300

    Remove extra url path

--- app/backend/src/routes/MatchRouter.ts
+++ app/backend/src/routes/MatchRouter.ts
@@ -4,8 +4,8 @@ import MatchController from '../controllers/MatchController';
 
 const router = Router();
 
-router.patch('/matches/:id/finish', MatchController.update);
-router.post('/matches', validToken, MatchController.create);
-router.get('/matches', MatchController.matches);
+router.patch('/:id/finish', MatchController.update);
+router.post('/', validToken, MatchController.create);
+router.get('/', MatchController.matches);
 
 export default router;
