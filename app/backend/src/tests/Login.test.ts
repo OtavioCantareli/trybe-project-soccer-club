040efb7f80ee21e456799931c00c630d1b07f84d
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-08-25 17:36:36 -0300

    More tests

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -1,8 +1,8 @@
 import * as chai from "chai";
 import * as sinon from "sinon";
+import { app } from "../app";
 // @ts-ignore
 import chaiHttp = require("chai-http");
-import { app } from "../app";
 
 import User from "../database/models/UserModel";
 import IntUser from "../interfaces/User";
@@ -24,6 +24,16 @@ describe("LOGIN", () => {
     username: "xablau",
   };
 
+  const noEmail = {
+    email: "",
+    password: "xablau",
+  };
+
+  const noPassword = {
+    email: "xablau@xablau.com",
+    password: "",
+  };
+
   beforeEach(async () => {
     sinon.stub(User, "findOne").resolves(userMock as User);
   });
@@ -40,5 +50,19 @@ describe("LOGIN", () => {
   it("Returns token", async () => {
     chaiHttpResponse = await chai.request(app).post("/login");
     expect(chaiHttpResponse.body).to.be.an("object").with.key("token");
+  });
+
+  it("Returns 400 HTTP with no email", async () => {
+    const response = await chai.request(app).post("/login").send(noEmail);
+    const body = response.body;
+    expect(response.status).to.equal(400);
+    expect(body.message).to.equal("All fields must be filled");
+  });
+
+  it("Returns 400 HTTP with no password", async () => {
+    const response = await chai.request(app).post("/login").send(noPassword);
+    const body = response.body;
+    expect(response.status).to.equal(400);
+    expect(body.message).to.equal("All fields must be filled");
   });
 });
